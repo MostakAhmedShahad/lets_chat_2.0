fd400cf2aedf8f01fcf8d1e83524dfe02c0a1603
Mostak <mostakahmedshahad74@gmail.com>
2025-03-12 01:39:07 +0600

    message_model

---
+++ lib/models/message_model.dart
@@ -0,0 +1,22 @@
+class Message {
+  final String senderId;
+  final String receiverId;
+  final String message;
+  final DateTime timestamp;
+
+  Message({
+    required this.senderId,
+    required this.receiverId,
+    required this.message,
+    required this.timestamp,
+  });
+
+  factory Message.fromMap(Map<String, dynamic> data) {
+    return Message(
+      senderId: data['senderId'],
+      receiverId: data['receiverId'],
+      message: data['message'],
+      timestamp: data['timestamp'].toDate(),
+    );
+  }
+}
